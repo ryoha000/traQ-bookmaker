b3f4de191e0dca9942ddcbcec4ea843c11317bc1
ryoha000 <the7uya@gmail.com>
2024-04-21 16:19:32 +0900

    feat: 🎸 NS_MARIADB_USERNAME -> NS_MARIADB_USER

--- adapter/src/persistence/mariadb.rs
+++ adapter/src/persistence/mariadb.rs
@@ -13,16 +13,18 @@ fn must_get_env(key: &str) -> String {
 
 impl Db {
     pub async fn new() -> Db {
-        let username = must_get_env("NS_MARIADB_USERNAME");
+        let username = must_get_env("NS_MARIADB_USER");
         let password = must_get_env("NS_MARIADB_PASSWORD");
         let hostname = must_get_env("NS_MARIADB_HOSTNAME");
         let port = must_get_env("NS_MARIADB_PORT");
         let database = must_get_env("NS_MARIADB_DATABASE");
 
-        let mut opt = ConnectOptions::new(format!(
+        let dsn = format!(
             "mysql://{}:{}@{}:{}/{}",
             username, password, hostname, port, database
-        ));
+        );
+        println!("Connecting to MariaDB: {}", dsn);
+        let mut opt = ConnectOptions::new(dsn);
         opt.max_connections(100)
             .min_connections(5)
             .connect_timeout(Duration::from_secs(8))
